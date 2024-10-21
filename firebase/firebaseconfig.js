6001941024b707dff22c071dc521d2eeedafea43
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-10-21 09:05:41 -0700

    Get Started this project I get the user data and upload on the firebase cloud storage lets get started on Oct 21, 24

---
+++ firebase/firebaseconfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB5wQ_gSUkF14-_xY4hR4EbmfwXfE8HQJg",
+    authDomain: "upload-user-data-on-firebase.firebaseapp.com",
+    projectId: "upload-user-data-on-firebase",
+    storageBucket: "upload-user-data-on-firebase.appspot.com",
+    messagingSenderId: "504937220445",
+    appId: "1:504937220445:web:252116b730e3aa8e7780b4",
+    measurementId: "G-Y156D2E1NK"
+};
+
+const app = initializeApp(firebaseConfig);
