df1ddac432b74e20c4d1937ff0c3d64a5d906aa9
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-10-22 04:51:35 -0700

    This project build by react let get started and name is vacancy-page on Oct 22, 24

--- firebase/firebaseconfig.js
+++ firebase/firebaseconfig.js
@@ -3,13 +3,20 @@ import { getStorage, ref, uploadBytes, uploadBytesResumable, getDownloadURL } fr
 import { getFirestore, doc, getDoc, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.14.1/firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB5wQ_gSUkF14-_xY4hR4EbmfwXfE8HQJg",
-    authDomain: "upload-user-data-on-firebase.firebaseapp.com",
-    projectId: "upload-user-data-on-firebase",
-    storageBucket: "upload-user-data-on-firebase.appspot.com",
-    messagingSenderId: "504937220445",
-    appId: "1:504937220445:web:252116b730e3aa8e7780b4",
-    measurementId: "G-Y156D2E1NK"
+    // apiKey: "AIzaSyB5wQ_gSUkF14-_xY4hR4EbmfwXfE8HQJg",
+    // authDomain: "upload-user-data-on-firebase.firebaseapp.com",
+    // projectId: "upload-user-data-on-firebase",
+    // storageBucket: "upload-user-data-on-firebase.appspot.com",
+    // messagingSenderId: "504937220445",
+    // appId: "1:504937220445:web:252116b730e3aa8e7780b4",
+    // measurementId: "G-Y156D2E1NK",
+
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_API_ID
 };
 
 const app = initializeApp(firebaseConfig);
